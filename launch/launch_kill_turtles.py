855e93f16a5f0b3abd1a6ec109d22de5b25f569b
Nafis Molla <nafisfardin@gmail.com>
2023-01-15 16:47:57 -0500

    added launch file

---
+++ launch/launch_kill_turtles.py
@@ -0,0 +1,38 @@
+import launch
+import launch_ros.actions
+from launch_ros.actions import Node
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import ComposableNodeContainer
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess,
+                            LogInfo, RegisterEventHandler, TimerAction)
+from launch.conditions import IfCondition
+from launch.event_handlers import (OnExecutionComplete, OnProcessExit,
+                                OnProcessIO, OnProcessStart, OnShutdown)
+from launch.events import Shutdown
+from launch.substitutions import (EnvironmentVariable, FindExecutable,
+                                LaunchConfiguration, LocalSubstitution,
+                                PythonExpression)
+
+
+
+def generate_launch_description():
+
+
+    continuous_node_container = ComposableNodeContainer(
+            name='node_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='training',
+               plugin='kill_all_turtle_composition::kill_alive_turtles',
+                    name='kill_all_turtles',
+                    )
+            ],
+            output='screen',
+    )
+
+
+    return launch.LaunchDescription([continuous_node_container])
