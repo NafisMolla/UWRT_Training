182e0dadec0ce50ac8f15a90a7df226aacd37946
Nafis Molla <nafisfardin@gmail.com>
2023-01-14 21:10:15 -0500

    task1 maybe done, cmake weird

---
+++ include/training/kill_alive_turtles.hpp
@@ -0,0 +1,26 @@
+#include <rclcpp/rclcpp.hpp>
+#include <turtlesim/srv/kill.hpp>
+
+namespace kill_all_turtle_composition{
+
+
+class kill_all_turtle_service_call: public rclcpp::Node{
+
+//Constructor
+public: 
+    explicit kill_all_turtle_service_call(const rclcpp::NodeOptions &options);
+
+private:
+//client pointer
+rclcpp::Client<turtlesim::srv::Kill>::SharedPtr client_;
+
+//array with all the possible names for the turtles
+std::vector<std::string> turtle_name = {"turlte1","moving_turtle","stationary_turtle"};
+
+
+void call_kill_service();
+
+};
+
+
+}
---
+++ src/kill_alive_turtles.cpp
@@ -0,0 +1,40 @@
+#include <include/training/kill_alive_turtles.hpp>
+
+namespace kill_all_turtle_composition{
+
+    //making the constructor
+    kill_all_turtle_service_call::kill_all_turtle_service_call(const rclcpp::NodeOptions &options)
+    : Node("kill_all_turtles_service_call", options){
+        //this is the service we want to call
+        client_ = this->create_client<turtlesim::srv::Kill>("/kill");
+        
+    }
+
+
+    //making the kill()
+    void kill_all_turtle_service_call::call_kill_service(){
+
+        //wait for the server to be up
+         while (!client_->wait_for_service(std::chrono::seconds(1)))
+        {
+            RCLCPP_WARN(this->get_logger(), "Waiting for the server to be up...");
+        }
+
+        //for every possible turtle available we will call the kill command on it
+
+        for(std::string name:turtle_name){
+            RCLCPP_INFO(this->get_logger(), "%s",name);
+        }
+
+
+
+    }
+    
+
+
+
+}
+
+#include <rclcpp_components/register_node_macro.hpp>
+
+RCLCPP_COMPONENTS_REGISTER_NODE(kill_all_turtle_composition::kill_all_turtle_service_call)
